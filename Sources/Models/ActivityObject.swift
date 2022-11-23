e5ac3241c94ef836b6314ffb13a5684b46671232
cionuigbo <cionuigbo@gmail.com>
2022-11-23 12:27:18 -0500

    Adding updateText to translate to different languages

--- Sources/Models/ActivityObject.swift
+++ Sources/Models/ActivityObject.swift
@@ -84,11 +84,6 @@ public enum ActivityObject: ActivityObjectProtocol {
     public static func missed() -> ActivityObject {
         return .text("!missed_reference")
     }
-    
-    public func updateText(_ updatedText:String){
-        text = updatedText
-       // self.original.
-    }
 }
 
 extension ActivityObject {
